8296dff5ea374fcd5f14fbcb44cac517080c8820
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2020-09-15 13:12:57 +0000

    Test

---
+++ src/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let comp;
+</script>
+
+<header>
+    <h1>Welcome to {comp}</h1>
+    <hr> 
+</header>
+
+<style>
+    header {
+		text-align: left;
+		padding: 1em;
+		max-width: 90%;
+		margin: 0 auto;
+	}
+
+    h1 {
+		color: #00ff00;
+		text-transform: uppercase;
+		font-size: 5em;
+		font-weight: 100;
+	}
+</style>
